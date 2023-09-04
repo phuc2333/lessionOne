a3df0b7c285bda2e159146594f788a5a438b41ac
- <phuc2333>
2023-09-04 22:06:32 +0700

    'init'

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <Tutorial/>
+</template>
+
+<script>
+import Tutorial from '@/components/Tutorial.vue'
+export default {
+  components: { Tutorial },
+  name: 'IndexPage'
+}
+</script>
